1db69d3840d24381b9e84e0507dbca828bacc533
AntonyThomasCGI <55574860+AntonyThomasCGI@users.noreply.github.com>
2020-07-02 19:18:19 +1200

    Add files via upload

---
+++ modules/ModuleBase.py
@@ -0,0 +1,53 @@
+import pymel.core as pm
+
+from .. import utils
+
+## --------------------------------------------------------------------------------
+'''
+
+	MODULEBASE.PY
+	contains base class for creating rig modules
+
+'''
+
+## --------------------------------------------------------------------------------
+
+class ModuleException( Exception ):
+	pass
+
+class Module( object ):
+
+	"""
+	Inherrit from this class ..blah blah
+
+	"""
+
+	def __init__( ):
+	pass
+	# end def __init__( self ):
+
+
+# 	def __str__( self ):
+# 		return 'rb.{}({})'.format( self.__class__.__name__, self.name )
+# 	# end def __str__( self ):
+
+
+# 	def __repr__( self ):
+# 		return self.__str__()
+# 	# end def __repr__( self ):
+
+
+# 	##-----------------------------------------------------------------------------
+# 	def build( self ):
+# 		raise ModuleBaseException('Invalid subclass-- build() function not '
+# 			+'implemented.')
+
+
+# 	##-----------------------------------------------------------------------------
+# 	def buildCleanup( self ):
+# 		## eg; transfers custom attrs from module root jnt
+# 		pass
+# 	#end def buildCleanup( self ):
+
+# # end class ModuleBase( Object ):
+ 
---
+++ modules/SimpleFk.py
@@ -0,0 +1,27 @@
+import pymel.core as pm
+
+from .ModuleBase import RBModule
+from .. import utils
+from ..userPrefs import user
+
+## --------------------------------------------------------------------------------
+'''
+
+	SIMPLEFK.PY
+	simple fk chain
+
+'''
+## --------------------------------------------------------------------------------
+
+
+class SingleChain( RBModule ):
+
+	def __init__( self, length, **kwargs ):
+		super( SingleChain, self ).__init__( length, **kwargs )
+
+	def makeBind( self ):
+
+		self.chain = utils.makeJointChain( self.length, self.name, user.prefs( 
+													'bind-skeleton-suffix' ) )
+	# end def makeBind( self ):
+# end class SingleChain( RBModule ):
