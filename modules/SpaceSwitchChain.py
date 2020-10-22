4d010d6fff41a6c9bb755ba178634fd3658258a6
AntonyThomasCGI <iamantonythomas1@gmail.com>
2020-10-22 23:11:17 +1300

    now distributes space blend along chain

--- modules/SpaceSwitchChain.py
+++ modules/SpaceSwitchChain.py
@@ -55,5 +55,40 @@ class SpaceSwitchChain(SimpleFk):
 		global_dm = pm.createNode('decomposeMatrix', n='{}_space_dcmpM'.format(self.name))
 		wt_add.matrixSum >> global_dm.inputMatrix
 		global_dm.outputRotate >> self.ctrlList[-1].null.rotate
+
+		if len(self) > 2:
+			inv_mm = pm.createNode('multMatrix', n='{}_invCtrl01_multM'.format(self.name))
+			wt_add = pm.createNode('wtAddMatrix', n='{}_distributeSpace_wtAdM'.format(self.name))
+			blend_two = pm.createNode('blendTwoAttr', n='{}_blend_two'.format(self.name))
+			subtract = pm.createNode('plusMinusAverage', n='{}_min_pma'.format(self.name))
+
+			inv_mm.matrixSum >> wt_add.wtMatrix[0].m
+			blend_two.output >> wt_add.wtMatrix[0].w
+			subtract.output1D >> wt_add.wtMatrix[1].w
+			blend_two.output >> subtract.input1D[1]
+			self.ctrlList[-1].ctrl.spaceBlend >> blend_two.attributesBlender
+			self.globalPlug >> inv_mm.matrixIn[0]
+			self.ctrlList[0].ctrl.worldInverseMatrix[0] >> inv_mm.matrixIn[1]
+
+			inv_mm.matrixIn[2].set(self.ctrlList[0].wMatrix)
+			subtract.setAttr('operation', 2)
+			subtract.input1D[0].set(1)
+			wt_add.wtMatrix[1].m.set(pm.dt.Matrix())
+			blend_two.input[0].set(1.0 / (len(self) - 2.0))
+			blend_two.input[1].set(0)
+
+			for i in range(1, (len(self) - 1)):
+				this_mm = pm.createNode('multMatrix', n='{0}_null{1:02d}_const_multM'.format(self.name, (i + 1)))
+				this_dm = pm.createNode('decomposeMatrix', n='{0}_null{1:02d}_const_dcmpM'.format(self.name, (i + 1)))
+
+				this_mm.matrixIn[0].set((
+					pm.dt.TransformationMatrix(
+						self.ctrlList[i].null.matrix.get())).asRotateMatrix()
+				)
+
+				wt_add.matrixSum >> this_mm.matrixIn[1]
+				this_mm.matrixSum >> this_dm.inputMatrix
+				# wt_add.matrixSum >> this_dm.inputMatrix
+				this_dm.outputRotate >> self.ctrlList[i].null.rotate
 	# end def build():
 # end class SpaceSwitchChain():
